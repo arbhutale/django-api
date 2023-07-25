1f9d9567300f533c2c2da9c18e6a8c412693ad3c
Anil Kumar <anil.bhutale@hedgehog.com>
2023-07-25 19:14:18 +0000

    first commit

--- core/settings.py
+++ core/settings.py
@@ -181,7 +181,7 @@ ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 TESTING = False
 TEST_RUNNER = "core.test_runner.CoreTestRunner"
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
